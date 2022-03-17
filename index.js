889c01c4e4c26f0d12adcf148ed3193245252731
MUHAMMADANWAAR101 <72974106+MUHAMMADANWAAR101@users.noreply.github.com>
2022-03-18 00:35:03 +0500

    3rd Commit intional tutorial Compelete

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const port = 8000
+const port = process.env.port || 8000
 const express = require('express')
 const axios = require('axios')
 const cheerio = require('cheerio')
@@ -7,72 +7,72 @@ const { response } = require('express')
 
 const app = express()
 
-const websites =[
+const websites = [
     {
-        name:'thetimes',
-        address:'https://www.thetimes.co.uk/environment/climate-change',
-        base:''
+        name: 'thetimes',
+        address: 'https://www.thetimes.co.uk/environment/climate-change',
+        base: ''
     },
     {
-        name:'theguardian',
-        address:'https://www.theguardian.com/environment/climate-crisis',
-        base:''
+        name: 'theguardian',
+        address: 'https://www.theguardian.com/environment/climate-crisis',
+        base: ''
     },
     {
-        name:'telegraph',
-        address:'https://www.telegraph.co.uk/climate-change/',
-        base:'https://www.telegraph.co.uk'
+        name: 'telegraph',
+        address: 'https://www.telegraph.co.uk/climate-change/',
+        base: 'https://www.telegraph.co.uk'
     }
 ]
-const articles= []
+const articles = []
 
 websites.forEach(website => {
     axios.get(website.address)
-    .then(response =>{
-        const  html = response.data
-        const $ = cheerio.load(html)
-        $ ('a:contains("climate")',html).each(function(){
-            const title = $(this).text()
-            const url = $(this).attr('href')
-            articles.push({
-                title,
-                url: website.base + url,
-                source: website.name
+        .then(response => {
+            const html = response.data
+            const $ = cheerio.load(html)
+            $('a:contains("climate")', html).each(function () {
+                const title = $(this).text()
+                const url = $(this).attr('href')
+                articles.push({
+                    title,
+                    url: website.base + url,
+                    source: website.name
+                })
             })
         })
-    })
 })
 
-app.get('/',(req,res)=> {
+app.get('/', (req, res) => {
     res.json('welcome to climate change api')
 })
 
-app.get('/news',(req,res) => {
-res.json(articles)
+app.get('/news', (req, res) => {
+    res.json(articles)
 })
-app.get('/news/:websiteid',async (req,res) => {
+app.get('/news/:websiteid', (req, res) => {
     const websiteid = req.params.websiteid
 
-  const websiteAddress = websites.filter(website => website.name == websiteid)[0].address
-  const websiteBase =websites.filter(website => website.name == websiteid)[0].base
+    const websiteAddress = websites.filter(website => website.name == websiteid)[0].address
+    const websiteBase = websites.filter(website => website.name == websiteid)[0].base
 
     axios.get(websiteAddress)
-    .then(response=>{
-        const html =response.data
-        const $ = cheerio.load(html)
-        const specificArticles =[]
-        $('a:contains("climate")',html).each(function(){
-            const title = $(this).text()
-            const url =$(this).attr('href')
-            specificArticles.push({
-                title,
-                url:websiteBase + url,
-                source:websiteid
+        .then(response => {
+            const html = response.data
+            const $ = cheerio.load(html)
+            const specificArticles = []
+            $('a:contains("climate")', html).each(function () {
+                const title = $(this).text()
+                const url = $(this).attr('href')
+                specificArticles.push({
+                    title,
+                    url: websiteBase + url,
+                    source: websiteid
 
+                })
             })
-        })
-        res.json(specificArticles)
-    }).catch(err => console.log(err))
+            res.json(specificArticles)
+        }).catch(err => console.log(err))
 })
 
-app.listen(port,() =>console.log('server run on port ${PORT}'))
+app.listen(port, () => console.log('server run on port ${PORT}'))
