e5ba493aa3117f3efbbd3a779d45b763a51233a4
MUHAMMADANWAAR101 <72974106+MUHAMMADANWAAR101@users.noreply.github.com>
2022-03-18 00:05:08 +0500

    2nd commit

--- index.js
+++ index.js
@@ -3,29 +3,30 @@ const express = require('express')
 const axios = require('axios')
 const cheerio = require('cheerio')
 const res = require('express/lib/response')
+const { response } = require('express')
 
 const app = express()
 
-const webites =[
+const websites =[
     {
-        name:'The Times',
+        name:'thetimes',
         address:'https://www.thetimes.co.uk/environment/climate-change',
         base:''
     },
     {
-        name:'The Guardian',
+        name:'theguardian',
         address:'https://www.theguardian.com/environment/climate-crisis',
         base:''
     },
     {
-        name:'Telegraph',
+        name:'telegraph',
         address:'https://www.telegraph.co.uk/climate-change/',
         base:'https://www.telegraph.co.uk'
     }
 ]
 const articles= []
 
-webites.forEach(website => {
+websites.forEach(website => {
     axios.get(website.address)
     .then(response =>{
         const  html = response.data
@@ -48,20 +49,30 @@ app.get('/',(req,res)=> {
 
 app.get('/news',(req,res) => {
 res.json(articles)
-    //   axios.get('https://www.theguardian.com/environment/climate-crisis')
- //   .then((response) => {
- //       const html = response.data
- //      const $ = cheerio.load(html)
- //      $('a:contains("climate")',html).each(function(){
- //         const title =  $(this).text()
- //         const url = $(this).attr('href')
- //         articles.push({
- //             title,
- //             url
- //         })  
- //      })
- //      res.json(articles)
- //   }).catch((err) =>console.log(err))
+})
+app.get('/news/:websiteid',async (req,res) => {
+    const websiteid = req.params.websiteid
+
+  const websiteAddress = websites.filter(website => website.name == websiteid)[0].address
+  const websiteBase =websites.filter(website => website.name == websiteid)[0].base
+
+    axios.get(websiteAddress)
+    .then(response=>{
+        const html =response.data
+        const $ = cheerio.load(html)
+        const specificArticles =[]
+        $('a:contains("climate")',html).each(function(){
+            const title = $(this).text()
+            const url =$(this).attr('href')
+            specificArticles.push({
+                title,
+                url:websiteBase + url,
+                source:websiteid
+
+            })
+        })
+        res.json(specificArticles)
+    }).catch(err => console.log(err))
 })
 
 app.listen(port,() =>console.log('server run on port ${PORT}'))
